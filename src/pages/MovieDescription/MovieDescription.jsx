f322a6b061c83c6d20029a72b4510750f128d358
Daniel Idoko <danielidoko46@gmail.com>
2025-09-12 22:44:31 +0100

    add searchinput to all routes

--- src/pages/MovieDescription/MovieDescription.jsx
+++ src/pages/MovieDescription/MovieDescription.jsx
@@ -6,15 +6,18 @@ import MovieDetail from "../../components/MovieDetail";
 import { lazy } from "react";
 import { Suspense } from "react";
 import useFetchMovies from "../../store/useFetchMovies";
+import SearchInput from '../../components/SearchInput'
 
 // Help Generate random id's for movies to prevent colliding id's
 import { v4 as uuid4 } from "uuid";
+import Main from "../../store/main";
 
 const SimilarMovies = lazy(() =>
   import("../../components/SimilarMovies/SimilarMovies")
 );
 
 const MovieDescription = () => {
+  // zustand states
   const {
     fetchAllMovies,
     filteredMovies,
@@ -24,6 +27,8 @@ const MovieDescription = () => {
     isLoading,
     error: errorMessage,
   } = useFetchMovies();
+
+  const { searchBarVisible } = Main();
 
   const [movieData, setMovieData] = useState(null);
   // Get the state/data from the params function
@@ -54,24 +59,39 @@ const MovieDescription = () => {
   }, []);
 
   return (
-    <div className="w-full h-full md:mt-20 p-3 md:p-10">
-      {isLoading ? (
-        <Spinner />
-      ) : errorMessage ? (
-        <p className="text-red-600 text-small md:text-medium">{errorMessage}</p>
-      ) : movieData ? (
-        <>
-          <MovieDetail movieData={movieData} key={uuid4()} />
-
-          {/* Similar movies section */}
-          <h3 className="p-3 text-white md:text-medium mt-3">Similar Movies</h3>
-          <Suspense fallback={<Spinner />}>
-            <SimilarMovies movie={movieData} />
-          </Suspense>
-        </>
-      ) : (
-        <Spinner />
+    <div className="w-full h-full md:mt-20">
+      {/* Search bar */}
+      {searchBarVisible && (
+        <div className="w-full h-fit fixed top-0 left-0 md:hidden p-2 flex items-center justify-center z-20">
+          <SearchInput />
+        </div>
       )}
+
+      <div className={`pl-3 md:pl-10 ${
+          searchBarVisible && "mt-13"
+        }`}>
+        {isLoading ? (
+          <Spinner />
+        ) : errorMessage ? (
+          <p className="text-red-600 text-small md:text-medium">
+            {errorMessage}
+          </p>
+        ) : movieData ? (
+          <>
+            <MovieDetail movieData={movieData} key={uuid4()} />
+
+            {/* Similar movies section */}
+            <h3 className="p-3 text-white md:text-medium mt-3">
+              Similar Movies
+            </h3>
+            <Suspense fallback={<Spinner />}>
+              <SimilarMovies movie={movieData} />
+            </Suspense>
+          </>
+        ) : (
+          <Spinner />
+        )}
+      </div>
     </div>
   );
 };
