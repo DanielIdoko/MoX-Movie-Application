5078ca7da7091b396f1a3eee0f0702f64f8ecb09
Daniel Idoko <danielidoko46@gmail.com>
2025-09-14 00:12:24 +0100

    added star for recommended searches

--- src/components/RecommendedSearches.jsx
+++ src/components/RecommendedSearches.jsx
@@ -17,8 +17,9 @@ const RecommendedSearches = () => {
       <ul className="w-full h-fit bg-gray-900/50 border-1 border-gray-800 p-2 grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-1 rounded-md mt-3">
         {popularMovies &&
           popularMovies.slice(0, 12).map((movie) => (
-            <li className="text-small text-gray-500 cursor-pointer hover:text-gray-400 transition duration-200 ease-in">
-              <Link to={`/movie/${movie.id}`}>{movie.originalTitle}</Link>
+            <li className="text-small text-gray-500 cursor-pointer hover:text-gray-400 flex items-center justify-start gap-2 transition duration-200 ease-in">
+              <Link to={`/movie/${movie.id}`}>{movie.originalTitle.length > 20 ? movie.originalTitle.slice(0, 15) + "..." : movie.originalTitle}</Link>
+              <span className="text-small">⭐{movie.averageRating}</span>
             </li>
           ))}
       </ul>
